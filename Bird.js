e25734d43d46cc8e0ee68191ab7c9669f90a41c8
Khoa Đậu <khoakomlem@gmail.com>
2023-02-19 01:42:44 +0700

    change constructor

--- Bird.js
+++ Bird.js
@@ -1,13 +1,20 @@
 class Bird {
-    lift = -10;
-    velocity = 0;
-    gravity = 0.5;
-    size = 30;
-    color = 255;
-
-    constructor({x = 60, y = 300} = {}) {
+    constructor({
+        x = 60,
+        y = 300,
+        lift = -10,
+        velocity = 0,
+        gravity = 0.5,
+        size = 30,
+        color = 'yellow'
+    } = {}) {
         this.x = x;
         this.y = y;
+        this.lift = lift;
+        this.velocity = velocity;
+        this.gravity = gravity;
+        this.size = size;
+        this.color = color;
     }
 
     draw() {
@@ -16,20 +23,22 @@ class Bird {
     }
 
     flap() {
-        this.velocity = this.lift;
+        this.velocity = this.lift; // Apply the lift force to the bird
     }
 
     update() {
         this.velocity += this.gravity;
         this.y += this.velocity;
 
-        if (this.y + this.size > height) {
+        // If the bird hits the bottom of the screen, set the bird to the bottom of the screen and the velocity to 0
+        if (this.y + this.size > height) { 
             this.y = height - this.size;
             this.velocity = 0;
         }
 
+        // If the bird hits the top of the screen, set the bird to the top of the screen and the velocity to 0
         if (this.y < 0) {
-            this.y = 0;
+            this.y = 0; 
             this.velocity = 0;
         }
     }
--- Pipe.js
+++ Pipe.js
@@ -1,16 +1,22 @@
 class Pipe {
-    constructor({x = width, y = 0, w = 50, h = 100, gap = 160} = {}) {
+    constructor({
+        x = width,
+        y = 0,
+        width = 50,
+        height = 100,
+        gap = 160
+    } = {}) {
         this.x = x;
         this.y = y;
-        this.w = w;
-        this.h = h;
+        this.width = width;
+        this.height = height;
         this.gap = gap;
     }
 
     draw() {
-        fill(255);
-        rect(this.x, this.y, this.w, this.h);
-        rect(this.x, this.y + this.h + this.gap, this.w, height - this.h - this.gap);
+        fill('green');
+        rect(this.x, this.y, this.width, this.height);
+        rect(this.x, this.y + this.height + this.gap, this.width, height - this.height - this.gap);
     }
 
     update() {
@@ -18,12 +24,12 @@ class Pipe {
     }
 
     offscreen() {
-        return this.x < -this.w;
+        return this.x < -this.width;
     }
 
     hits(bird) {
-        if (bird.y < this.h || bird.y + bird.size > this.h + this.gap) {
-            if (bird.x + bird.size > this.x && bird.x < this.x + this.w) {
+        if (bird.y < this.height || bird.y + bird.size > this.height + this.gap) {
+            if (bird.x + bird.size > this.x && bird.x < this.x + this.width) {
                 return true;
             }
         }
