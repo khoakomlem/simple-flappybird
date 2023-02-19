69710d8099786a46fa5aa5f0ed71c5068c57c3cb
Khoa Đậu <khoakomlem@gmail.com>
2023-02-19 10:05:33 +0700

    change background, add restart scence, score system

--- game.js
+++ game.js
@@ -1,20 +1,34 @@
-const pipes = [];
-const bird = new Bird();
+let pipes, bird, score;
 
-function setup() {
-    createCanvas(1366, 768);
-
+function initial() {
+    pipes = [];
+    bird = new Bird();
+    score = 0;
     // Create 5 initial pipes on start
     for (let i = 0; i < 5; i++) {
         pipes.push(new Pipe({
             x: 500 + i * 300,
-            height: random(200, height - 200)
+            height: random(100, height - 200)
         }));
     }
 }
 
+function setup() {
+    createCanvas(1366, 768);
+    textAlign(CENTER, CENTER);
+
+    initial();
+}
+
 function draw() {
-    background(0); // Set the background to black
+    background(255); // Set the background to black
+    
+    strokeWeight(10);
+    stroke(80);
+    line(0, height - 100, width, height - 100); // Draw the ground
+
+    strokeWeight(3);
+    stroke("black");
 
     // If the first pipe is offscreen, remove it
     if (pipes[0] && pipes[0].offscreen()) {
@@ -34,15 +48,32 @@ function draw() {
     // If the bird hits the first pipe, change the bird's color to red
     if (pipes[0] && pipes[0].hits(bird)) {
         bird.color = 'red';
+        bird.draw();
+        fill('white');
+        strokeWeight(6);
+        stroke(80);
+        textSize(50);
+        text("PRESS R TO RESTART", width/2 , height/2);
+        frameRate(0);
     } else {
         bird.color = 'yellow';
     }
+
+    if (bird.x > pipes[0].x + pipes[0].width && !pipes[0].passed) {
+        score++;
+        pipes[0].passed = true;
+    }
+    fill('white');
+    strokeWeight(6);
+    stroke(80);
+    textSize(80);
+    text(score, width/2 , 100);
 
     // If the frame count is a multiple of 100, create a new pipe
     if (frameCount % 100 === 0) {
         pipes.push(new Pipe({
             x: pipes[pipes.length - 1].x + 300,
-            height: random(200, height - 200)
+            height: random(100, height - 200)
         }));
     }
 }
@@ -53,4 +84,8 @@ function mousePressed() {
 
 function keyPressed() {
     bird.flap();
+    if (keyCode === 82) {
+        initial();
+        frameRate(60);
+    }
 }
